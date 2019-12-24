c67fe91af5b8119e708b48cfe9056387e8177a63
Adam <adam.dlugosz.adam@gmail.com>
2019-12-24 23:02:46 +0100

    nawet nie wiem

--- rysowanie funkcji.py
+++ rysowanie funkcji.py
@@ -9,17 +9,18 @@ m = float(input("do kąta"))
 l = float(input("między kątami"))
 n = input("wybierz sposób rysowania")
 x = np.arange(k, m, l)
+plt.figure()
+plt.axvline(color="k", lw=2)
+plt.axhline(color="k", lw=2)
 for i in range(r):
     f = Expression(input("podaj wzór funkcji"), ["x"])
     Y.append(f)
 for f in Y:
     y = f(x)
-    if n == "linia ciągła":
-        plt.plot(x, y, label=str(f))
+    if n == "lc":
+        plt.plot(x, y, label=f)
     else:
-        plt.plot(x, y, 'g*', label=str(f))
+        plt.plot(x, y, 'g*', label=f)
 plt.legend()
 plt.grid()
-plt.axvline(color="k", lw=2)
-plt.axhline(color="k", lw=2)
 plt.show()
