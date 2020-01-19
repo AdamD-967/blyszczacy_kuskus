fc469420c2ad4efd514fef48a93965c1ee8e1316
Adam <adam.dlugosz.adam@gmail.com>
2020-01-19 12:28:48 +0100

    float
    zaokrąglanie float'ów

--- zaokrąglanie liczb.py
+++ zaokrąglanie liczb.py
@@ -1,17 +1,44 @@
-x = int(input("podaj liczbe (int)"))
-z = int(input("podaj ilość cyfr znaczących"))
+x = float(input("podaj liczbę"))
+z = int(input("podaj liczbę cyfr znaczących"))
+p = input("przed czy za przecinkiem")
 n = 0
 X = []
 for i in range(len(str(x))):
-    X.append(int(str(x)[n]))
+    X.append(str(x)[n])
     n = n+1
-if X[z] >= 5:
-    X[z-1] = X[z-1]+1
-q = 1
-for j in range(len(X)-z):
-    X[(z-1)+q] = 0
-    q = q+1
-o = ""
-for k in X:
-    o = o+str(k)
-print(int(o))
+Xp = []
+up = 0
+uz = X.index('.')+1
+while up < X.index('.'):
+    Xp.append(int(X[up]))
+    up = up+1
+if p == 'p':
+    print(Xp)
+    if Xp[z] >= 5:
+        Xp[z-1] = Xp[z-1]+1
+    q = 1
+    for j in range(len(Xp)-z):
+        Xp[(z-1)+q] = 0
+        q = q+1
+    o = ""
+    for k in Xp:
+        o = o+str(k)
+    o = o+'.0'
+else:
+    Xz = []
+    for i in range(len(X)-uz):
+        Xz.append(int(X[uz]))
+        uz = uz+1
+    if Xz[z] >= 5:
+        Xz[z-1] = Xz[z-1]+1
+    q = 1
+    for j in range(len(Xz)-z):
+        Xz[(z-1)+q] = 0
+        q = q+1
+    o = ""
+    for k in Xp:
+        o = o + str(k)
+    o = o+'.'
+    for l in Xz:
+        o = o+str(l)
+print(float(o))
