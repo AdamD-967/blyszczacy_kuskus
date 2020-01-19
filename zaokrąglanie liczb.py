fae26083ac5c1bda0e56f63657c23a3556394895
Adam <adam.dlugosz.adam@gmail.com>
2020-01-19 12:28:48 +0100

    float1
    zaokrąglanie float'ów (przed przecinkiem)

--- zaokrąglanie liczb.py
+++ zaokrąglanie liczb.py
@@ -1,17 +1,30 @@
-x = int(input("podaj liczbe (int)"))
-z = int(input("podaj ilość cyfr znaczących"))
+x = float(input("podaj liczbę"))
+z = int(input("podaj liczbę cyfr znaczących"))
+p = input("przed czy za przecinkiem")
 n = 0
 X = []
 for i in range(len(str(x))):
-    X.append(int(str(x)[n]))
+    X.append(str(x)[n])
     n = n+1
-if X[z] >= 5:
-    X[z-1] = X[z-1]+1
-q = 1
-for j in range(len(X)-z):
-    X[(z-1)+q] = 0
-    q = q+1
-o = ""
-for k in X:
-    o = o+str(k)
-print(int(o))
+Xp = []
+up = 0
+Xz = []
+r = X.index('.')-1
+uz = r+2
+if p == 'p':
+    for v in range(len(X)-(r+1)):
+        Xp.append(int(X[up]))
+        up = up+1
+    if Xp[z] >= 5:
+        Xp[z-1] = Xp[z-1]+1
+    q = 1
+    for j in range(len(Xp)-z):
+        Xp[(z-1)+q] = 0
+        q = q+1
+    o = ""
+    for k in Xp:
+        o = o+str(k)
+    o = o+'.0'
+    print(o)
+else:
+    print("tymczasowy brak funkcji")
